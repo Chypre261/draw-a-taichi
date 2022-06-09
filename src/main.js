273578b7d16b005762b8762123c43da85056bc26
chypre <simone@SimonedeMac-mini.local>
2022-06-09 22:13:45 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,78 @@
+let html = document.querySelector("#html");
+let style = document.querySelector("#style");
+
+let string = `/* Let's see how to draw a Tai Chi
+ * with css and javascript.
+ * make a div
+ **/
+#div1{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+/* 
+ * make a cirvle
+ **/
+#div1{
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border: none;
+}
+/* 
+ * black & white
+ **/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* add to little balls */
+#div1::before{
+    width: 100px;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+}
+`;
+let string2 = "";
+let n = 0;
+
+let step = () => {
+  setTimeout(() => {
+
+    // optimize newline and space
+    if (string[n] === "\n") {
+      string2 += "<br>";
+    } else if (string[n] === " ") {
+      string2 += "&nbsp;";
+    } else {
+      string2 += string[n];
+    }
+
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n);
+
+    window.scrollTo(0, 99999);
+    html.scrollTo(0, 99999);
+
+    if (n < string.length - 1) {
+      // if n is not the last one, go on
+      n += 1;
+      step();
+    }
+  }, 50);
+};
+
+step();
